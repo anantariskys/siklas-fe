e439bb6e8b90c42312fe8b13194bf9862d3511ef
anantariskys <gamersindo555@gmail.com>
2025-11-21 18:15:01 +0700

    feat(api-response): add pagination response type and make data optional

--- src/types/api-response.ts
+++ src/types/api-response.ts
@@ -1,4 +1,4 @@
-export interface SuccessResponse<T> {
+export interface SuccessResponse<T = void> {
   success: true;
   message: string;
   data: T;
@@ -8,4 +8,16 @@ export interface ErrorResponse {
   success: false;
   message: string;
   errors: string;
+}
+
+export interface SuccessPaginationResponse<T> {
+  success: true;
+  message: string;
+  data: T[];
+  pagination: {
+    total: number;
+    per_page: number;
+    current_page: number;
+    last_page: number;
+  };
 }
